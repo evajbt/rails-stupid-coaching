0ad1f8c834584561f27ed9138d7b0a8e9d69700b
Eva Joubert <evajoubert@live.fr>
2023-05-13 12:43:27 +0200

    Stupid Coaching avec front

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/ask', to: 'questions#ask'
-  get '/answer', to: 'questions#answer'
+  post '/answer', to: 'questions#answer'
 end
